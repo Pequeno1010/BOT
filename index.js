f373eb7daed8e2ce582dd85ab304e10a28c9aa62
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-04-14 22:07:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Client } = require('discord.js');
 const client = new Client({DisableEveryone: true});
 
-bot.on('ready', async () =>{
+client.on('ready', async () =>{
   console.log('Le bot est lancé.');
-  bot.user.setActivity('Regarde Seattle RP');
+  client.user.setActivity('Regarde Seattle RP');
 });
 
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 
 
