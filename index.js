0d91d93deba989f2cccb8e2d56b084d42f85847b
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-04-14 14:23:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client } = require("discord.js");
+const client = new Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+
+  //message serveur on/reboot
+
+  if (msg.content === "ping") msg.channel.send("pong");
+  if (msg.content === "!on") msg.channel.send("@everyone, serveur ON");
+  if (msg.content === "!reboot") msg.channel.send("@everyone, serveur REBOOT");
+});
+
