bd7c116ac9adf90f13f0674fa756525f006e3182
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-04-20 19:53:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on("message", msg => {
 
   if (msg.content === "ping") msg.channel.send("pong");
   if (msg.content === "!on") msg.channel.send("@everyone, serveur ON");
-  msg.channel.send("");
+  if(msg.content === "!on") msg.channel.send("v")
   if (msg.content === "!reboot") msg.channel.send("@everyone, serveur REBOOT");
 });
 
