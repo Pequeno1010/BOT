0a9db078f87ac2f70b5721e56b982e0beb8cc9be
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-06-06 21:00:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("message", msg => {
 
   //message serveur on/reboot
 
-  if (msg.content === "pin") <arrivée>.send(msg.content);
+  if (msg.content === "pin") "arrivée".send(msg.content);
   if (msg.content === "!on") msg.channel.send("@everyone\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n                               ✅SERVEUR ON✅\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n 📌 ip serveur: 164.132.201.141:27190");
   if (msg.content === "!reboot") msg.channel.send("@everyone\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n                           ⛔SERVEUR REBOOT⛔\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n Merci de bien vouloir patienter. 🙂");
 });
