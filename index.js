bfad74c52cac78b9c10a64641904b225f5977717
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-06-07 02:45:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("ready", async () => {
 
 client.on("message", msg => {
   const channel = client.channels.cache.get("698529212696625272");
-  if (!channel) return;
+  
   if (msg.content === "!on") msg.channel.send("@everyone\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n                               :white_check_mark:SERVEUR ON:white_check_mark:\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n :pushpin: ip serveur: 164.132.201.141:27190");
   if (msg.content === "!reboot") msg.channel.send("@everyone\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n                           :no_entry:SERVEUR REBOOT:no_entry:\n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n Merci de bien vouloir patienter. :slight_smile:");
 });
