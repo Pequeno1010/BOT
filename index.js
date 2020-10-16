791950847d6b01b9c5eb9c9ea549be16274cf6a3
Tamiaclacla1010 <clarence.yon@gmail.com>
2020-10-16 23:53:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,47 +63,5 @@ client.on("guildMemberRemove", member => {
   channel.send(`${member} Bonne continuation`);
 });
 
-// giveaway
 
-client.on('message', message => {
-  let args = message.content.sebstring(prefix.length).split(" ")
 
-  if (message.content.startsWith(`${prefix}giveaway`)) {
-      let time = args[1]
-      if (!time) return message.channel.send(`You did not specify your time!`);
-
-      if (
-        !args[1].endsWith("d") &&
-        !args[1].endsWith("h") &&
-        !args[1].endsWith("m") &&
-        !args[1].endsWith("s") 
-      )
-        return message.channel.send("You need to use d (days), h (hours), m (minutes), or s (seconds)")
-
-        let gcchannel = message.mentions.channels.first();
-        if (!channel) return message.channel.send("I can\'t find that channel in the server!")
-
-        let prize = args.slice(3).join(" ")
-        if (!prize) return message?channel.send('Arguement Missing! What is the prize?')
-
-        message.delete()
-        channel.send(":tada: **NEW GIVEAWAY** :tada:")
-        let gembed = new DiscordAPIError.messageEmbed()
-            .setTitle('New Giveaway!')
-            .setColor("RAMDOM")
-            .setDescription(`React with :tada: to enter the giveaway!\nHosted By: **${message.author}**\nTime: **${time}**`)
-            .setTimestamp(Date.now + ms(args[1]))
-            .setFooter('Will end at')
-            let m = await channel.send(gembed)
-            ms.react("🎉")
-            setTimeout(() => {
-              if (m.reactions.cahche.get("🎉").count <= 1) {
-                return message.channel.send("Not enough people reacted for me to draw a winner!")
-              }
-
-              let winner = m.reactions.chache.get("🎉").users.cache.filter((u) => !u.bot).random();
-              channel.send(`Congratulation ${winner}! You just Won the **${prize}**!`
-              );
-            }, ms(args[1]));
-  }
-}) 
