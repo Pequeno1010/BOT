b16bbfd9ddb45f1d1781835505ecbe24098ca225
Tamiaclacla1010 <clarence.yon@gmail.com>
2021-03-05 20:54:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, DiscordAPIError } = require("discord.js");
+const { Client, DiscordAPIError, PermissionOverwrites } = require("discord.js");
 const client = new Client();
 
 client.on("ready", async () => {
@@ -72,4 +72,94 @@ client.on("guildMemberRemove", member => {
   channel.send(`${member}a préféré nous quitter\n https://tenor.com/view/peterson-farm-bros-dinosaur-truck-atv-gif-8466996`);
 });
 
+//// TICKET ////
 
+// ticket.js
+const config = require ( '../config.json' ) ,
+    fs = exiger ( 'fs' ) ,
+    Discord = require ( 'discord.js' )
+ 
+module. exportations  =  {
+    run : async ( message , args , client )  =>  {
+        if  ( Object . values ( client. db . tickets ) . some ( ticket => ticket. author  === message. author . id ) )  renvoie un message. canal . send ( 'Vous avez déjà un ticket d \' ouvert. ' )
+        canal const = message d'attente. guilde . canaux . create ( `ticket $ { message. author . username } ` ,  {
+            tapez :  'texte' ,
+            parent : config. billet . catégorie ,
+            permissionOverwrites :  [ {
+                id : message. guilde . id ,
+                refuser :  "VIEW_CHANNEL"
+            } ,  {
+                id : message. auteur . id ,
+                autoriser :  "VIEW_CHANNEL"
+            } , ... config . billet . rôles . carte ( id =>  ( {
+                id ,
+                autoriser :  "VIEW_CHANNEL"
+            } ) ) ]
+        } )
+        client. db . billets [ channel. id ]  =  {
+            auteur : message. auteur . identifiant
+        }
+        fs. writeFileSync ( './db.json' , JSON. stringify ( client. db ) )
+        canaliser. envoyer ( nouveau Discord. MessageEmbed ( )
+            . setDescription ( `Bonjour $ { message. membre } , bienvenue dans votre ticket. Nous allons nous occuper de vous.` ) )
+        un message. canal . send ( `Votre ticket $ { channel } a été créé ! ` )
+    } ,
+    nom :  'ticket' ,
+    guildOnly :  vrai
+}
+ 
+// close.js
+const fs = exiger ( 'fs' )
+ 
+module. exportations  =  {
+    run : async ( message , args , client )  =>  {
+        canal const = message. mentions . canaux . premier ( )  || un message. canaliser
+        if  ( ! client. db . tickets [ channel. id ] )  renvoie un message. canal . send ( 'Ce salon n \' est pas un ticket. ' )
+        if  ( ! message. membre . hasPermission ( 'MANAGE_MESSAGES' )  && client. db . tickets [ channel. id ] . author  ! == message. author . id )  renvoie un message. canal . send ( 'Vous n \' avez pas la permission de fermer ce ticket. ' )
+        supprimer le client. db . billets [ channel. id ]
+        fs. writeFileSync ( './db.json' , JSON. stringify ( client. db ) )
+        attendre le message. canal . send ( `Le ticket $ { channel. name } a été fermé ! ` )
+        canaliser. supprimer ( )
+    } ,
+    nom :  'fermer' ,
+    guildOnly :  vrai
+}
+ 
+// db.json
+{ "warns" : { } , "tickets" : { } }
+ 
+// config.json
+{
+    "token" :  "votre token" ,
+    "préfixe" :  "!" ,
+    "salut" :  {
+        "canal" :  "718970110164992041" ,
+        "role" :  "718970160647635056"
+    } ,
+    "reactionRole" :  {
+        "721812281347932311" :  {
+            "emojis" :  [ {
+                "id" :  "719563468008718348" ,
+                "roles" :  "722404495371534358"
+            } ]
+        } ,
+        "721812680763244596" :  {
+            "amovible" :  vrai ,
+            "emojis" :  [ {
+                "nom" :  "💻" ,
+                "rôles" :  [ "722088168458813584" ,  "722088234963828747" ]
+            } ,  {
+                "nom" :  "🎮" ,
+                "roles" :  "722088255402672139"
+            } ]
+        }
+    } ,
+    "cooldown" :  {
+        "723998402395767288" : 1e4 ,
+        «723998419986808883» : 3e4
+    } ,
+    "ticket" :  {
+        "catégorie" :  "741691177807380552" ,
+        "rôles" :  [ "741690852719460463" ,  "741690932164034670" ]
+    }
+}
