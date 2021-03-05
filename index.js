5d4e876f355387dc01c0bd4aa1bb21a3bccebb05
Tamiaclacla1010 <clarence.yon@gmail.com>
2021-03-05 21:06:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("ready", async () => {
 //// message raid off activé/désactivé ////
 
 client.on("message", msg => {
-  const channel = client.channels.cache.get("765989231755132930");
+  const channel = client.channels.cache.get("698529212696625272");
   if (!channel) return;
   if (msg.content === "!raid off activé") channel.send("@everyone l'anti  raid off est maintenant activé.\n Bon jeu 😎");
   if (msg.content === "!raid off activé dans 5min") channel.send("@everyone Salut tout le monde je viens vous prévenir que l'anti raid off est activé dans 5 minutes");
@@ -73,92 +73,3 @@ client.on("guildMemberRemove", member => {
 });
 
 //// TICKET ////
-// ticket.js
-const config = require('../config.json'),
-    fs = require('fs'),
-    Discord = require('discord.js')
-
-module.exports = {
-    run: async (message, args, client) => {
-        if (Object.values(client.db.tickets).some(ticket => ticket.author === message.author.id)) return message.channel.send('Vous avez déjà un ticket d\'ouvert.')
-        const channel = await message.guild.channels.create(`ticket ${message.author.username}`, {
-            type: 'text',
-            parent: config.ticket.category,
-            permissionOverwrites: [{
-                id: message.guild.id,
-                deny: 'VIEW_CHANNEL'
-            }, {
-                id: message.author.id,
-                allow: 'VIEW_CHANNEL'
-            }, ...config.ticket.roles.map(id => ({
-                id,
-                allow: 'VIEW_CHANNEL'
-            }))]
-        })
-        client.db.tickets[channel.id] = {
-            author: message.author.id
-        }
-        fs.writeFileSync('./db.json', JSON.stringify(client.db))
-        channel.send(new Discord.MessageEmbed()
-            .setDescription(`Bonjour ${message.member}, bienvenue dans votre ticket. Nous allons nous occuper de vous.`))
-        message.channel.send(`Votre ticket ${channel} a été créé !`)
-    },
-    name: 'ticket',
-    guildOnly: true
-}
-
-// close.js
-const fs = require('fs')
-
-module.exports = {
-    run: async (message, args, client) => {
-        const channel = message.mentions.channels.first() || message.channel
-        if (!client.db.tickets[channel.id]) return message.channel.send('Ce salon n\'est pas un ticket.')
-        if (!message.member.hasPermission('MANAGE_MESSAGES') && client.db.tickets[channel.id].author !== message.author.id) return message.channel.send('Vous n\'avez pas la permission de fermer ce ticket.')
-        delete client.db.tickets[channel.id]
-        fs.writeFileSync('./db.json', JSON.stringify(client.db))
-        await message.channel.send(`Le ticket ${channel.name} a été fermé !`)
-        channel.delete()
-    },
-    name: 'close',
-    guildOnly: true
-}
-
-// db.json
-{"warns",{},"tickets",{}}
-
-// config.json
-{
-    "token", "votre token",
-    "prefix", "!",
-    "greeting", {
-        "channel": "718970110164992041",
-        "role": "718970160647635056"
-    },
-    "reactionRole", {
-        "721812281347932311": {
-            "emojis": [{
-                "id": "719563468008718348",
-                "roles": "722404495371534358"
-            }]
-        },
-        "721812680763244596": {
-            "removable": true,
-            "emojis": [{
-                "name": "💻",
-                "roles": ["722088168458813584", "722088234963828747"]
-            }, {
-                "name": "🎮",
-                "roles": "722088255402672139"
-            }]
-        }
-    },
-    "cooldown", {
-        "723998402395767288": 1e4,
-        "723998419986808883": 3e4
-    },
-    "ticket", {
-        "category": "741691177807380552",
-        "roles": ["741690852719460463", "741690932164034670"]
-    }
-}
